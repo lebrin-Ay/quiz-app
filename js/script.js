d2907c05808f839243c875aaa7298ada8d3403a3
Allen Morris <allenmorris140@gmail.com>
2023-04-21 13:32:55 +0100

    js update

--- js/script.js
+++ js/script.js
@@ -1,40 +1,123 @@
-const questions = [
-  {
-    questionText: "Commonly used data types DO NOT include:",
-    options: ["1. strings", "2. booleans", "3. alerts", "4. numbers"],
-    answer: "3. alerts",
-  },
-  {
-    questionText: "Arrays in JavaScript can be used to store ______.",
-    options: [
-      "1. numbers and strings",
-      "2. other arrays",
-      "3. booleans",
-      "4. all of the above",
-    ],
-    answer: "4. all of the above",
-  },
-  {
-    questionText:
-      "String values must be enclosed within _____ when being assigned to variables.",
-    options: ["1. commas", "2. curly brackets", "3. quotes", "4. parentheses"],
-    answer: "3. quotes",
-  },
-  {
-    questionText:
-      "A very useful tool used during development and debugging for printing content to the debugger is:",
-    options: [
-      "1. JavaScript",
-      "2. terminal/bash",
-      "3. for loops",
-      "4. console.log",
-    ],
-    answer: "4. console.log",
-  },
-  {
-    questionText:
-      "Which of the following is a statement that can be used to terminate a loop, switch or label statement?",
-    options: ["1. break", "2. stop", "3. halt", "4. exit"],
-    answer: "1. break",
-  },
-];
+const question = document.getElementById("question");
+const choices = Array.from(document.getElementsByClassName("choice-text"));
+
+let currentQuestion = {};
+let acceptingAnswers = false;
+let score = 0;
+let questionCounter = 0;
+let availableQuestions = [];
+
+
+let questions = [
+    {
+      question: "Commonly used data types DO NOT include:",
+      choice1: "1. strings",
+      choice2: "2. booleans",
+      choice3: "3. alerts",
+      choice4: "4. numbers",
+      answer: 3
+    },
+    {
+      question: "Arrays in JavaScript can be used to store ______.", 
+        choice1: "1. numbers and strings",
+        choice2: "2. other arrays",
+        choice3: "3. booleans",
+        choice4: "4. all of the above",
+      
+      answer: 4
+    },
+    {
+      question: "String values must be enclosed within _____ when being assigned to variables.",
+      choice1: "1. commas",
+      choice2:  "2. curly brackets",
+      choice3: "3. quotes",
+      choice4: "4. parentheses",
+      answer: 3,
+    },
+    {
+      question: "A very useful tool used during development and debugging for printing content to the debugger is:",
+        choice1: "1. JavaScript",
+        choice2: "2. terminal/bash",
+        choice3: "3. for loops",
+        choice4: "4. console.log",
+      answer: 4
+    },
+    {
+      question:
+        "Which of the following is a statement that can be used to terminate a loop, switch or label statement?",
+      choice1: "1. break",
+      choice2: "2. stop", 
+      choice3: "3. halt",
+      choice4: "4. exit",
+      answer: 1
+    },
+  ];
+
+const CORRECT_BONUS = 10;
+const MAX_QUESTIONS = 5;
+
+startGame = () => {
+    questionCounter = 0;
+    score = 0;
+    availableQuestions = [...questions];
+    getNewQuestion();
+
+};
+getNewQuestion = () => {
+    if (availableQuestions.length === 0 || questionCounter >= MAX_QUESTIONS){
+        //go to end page
+        return window.location.assign('/finished.html');
+    }
+    questionCounter++;
+    const questionIndex = Math.floor(Math.random() * availableQuestions.length);
+    currentQuestion = availableQuestions[questionIndex];
+    question.innerText = currentQuestion.question;
+
+    choices.forEach( choice =>{
+        const number = choice.dataset['number'];
+        choice.innerText = currentQuestion['choice' + number];
+    })
+
+    availableQuestions.splice(questionIndex, 1);
+
+    acceptingAnswers = true;
+};
+
+choices.forEach( choice => {
+    choice.addEventListener('click', e => {
+     if(!acceptingAnswers) return;
+
+     acceptingAnswers = false;
+     const selectedChoice = e.target;
+     const selectedAnswer = selectedChoice.dataset['number'];
+
+    const classToApply = 
+      selectedAnswer == currentQuestion.answer ? "correct" : "incorrect";
+    
+
+    selectedChoice.parentElement.classList.add(classToApply);
+
+    setTimeout(() => {
+        selectedChoice.parentElement.classList.remove(classToApply);
+        getNewQuestion();
+
+    }, 1000);
+     
+    });
+    var crct = document.getElementById('crct')
+    if(currentQuestion.answer == "correct"){
+        crct.style.display = 'block';
+        incrct.style.display = 'none';
+        
+    };
+
+    var incrct = document.getElementById('incrct');
+    if(currentQuestion.answer == "incorrect"){
+        incrct.style.display = 'block';
+        crct.style.display = 'none'
+        
+    };
+
+});
+
+startGame();
